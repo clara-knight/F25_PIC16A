410a72ad668e82aa7dfc5fd2a4e490b556d0605d
Clara Knight <cea144@ucla.edu>
2025-11-05 21:12:12 -0800

    Fixed iterator object. Finished homework 4

--- HW/4/hw4.py
+++ HW/4/hw4.py
@@ -26,19 +26,18 @@ class MatrixIterator:
     def __init__(self, Matrix):
         self.shape = Matrix.shape
         self.elements = Matrix.elements
-        self.i = 0
-        self.j = -1
+
+        self.j = 0
+        self.i = -1
 
     def __next__(self):
-        self.j += 1
+        self.i += 1
+        if self.i >= self.shape[0]:
+            self.i = 0
+            self.j += 1
 
         if self.j >= self.shape[1]:
-            self.j = 0
-            self.i += 1
-
-        if self.i >= self.shape[0]:
             raise StopIteration
-
         return self.elements[self.i][self.j]
 
 
@@ -57,8 +56,9 @@ class SquareMatrix(Matrix):
                 "Square matrix should have the same number of rows and columns"
             )
         super().__init__(elements, shape)
+        self.Trace()
 
     def Trace(self):
-        self.trace = 1
-        for i in self.shape[0]:
-            self.trace *= self.elements[i][i]
+        self.trace = 0
+        for i in range(0, self.shape[0]):
+            self.trace += self.elements[i][i]
