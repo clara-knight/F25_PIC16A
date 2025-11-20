fafce18d2a07386459ea40df9d5d73d656a932ba
Clara Knight <cea144@ucla.edu>
2025-11-19 22:56:32 -0800

    Finished HW5

--- HW/5/hw5.py
+++ HW/5/hw5.py
@@ -2,12 +2,6 @@ import numpy as np
 
 np.set_printoptions(precision=2, suppress=True)
 
-#   0: product id
-#   1: units sold
-#   2: unit price
-#   3: discount rate
-#   4: region code
-
 data = np.array(
     [
         [101, 5, 10.0, 0.1, 0],
@@ -27,9 +21,13 @@ data = np.array(
 def clean_data(data):
     cleaned_data = data.copy()
 
-    units_mask = cleaned_data[:, 1] != -1
-    price_mask = cleaned_data[:, 2] > 0
-    discount_mask = cleaned_data[:, 3] < 1
+    units_sold = cleaned_data[:, 1]
+    unit_price = cleaned_data[:, 2]
+    discount_rate = cleaned_data[:, 3]
+
+    units_mask = units_sold != -1
+    price_mask = unit_price > 0
+    discount_mask = discount_rate < 1
 
     median_units = np.median(cleaned_data[units_mask, 1])
     mean_prices = np.mean(cleaned_data[price_mask, 2])
@@ -40,9 +38,6 @@ def clean_data(data):
     cleaned_data[~discount_mask, 3] = mean_discount
 
     return cleaned_data
-
-
-cleaned = clean_data(data)
 
 
 # 2
@@ -75,6 +70,57 @@ def region_summary(data):
     return dict(zip(unique_regions, zip(*vals)))
 
 
-summary = region_summary(cleaned)
-
 # 4
+def adjust_prices(data):
+    adjusted_data = data.copy()
+
+    prices = adjusted_data[:, 2]
+    discount_rate = adjusted_data[:, 3]
+    regions = adjusted_data[:, 4]
+
+    region_mask = regions <= 1
+    discount_mask = discount_rate > 0.2
+
+    prices[region_mask] *= 1.05
+    prices[discount_mask] *= 0.9
+
+    return adjusted_data
+
+
+# EXERCISE 2
+
+
+class GradeSystem:
+    def __init__(self, hw, exam, hw_weight, exam_weights):
+        if sum(exam_weights) + hw_weight != 1:
+            raise ValueError("The sum of weights should be 1")
+        if exam.shape[1] != exam_weights.shape[0]:
+            raise ValueError(
+                "The number of exams and the number of weights do not match"
+            )
+        self.hw = hw
+        self.exam = exam
+        self.hw_weight = hw_weight
+        self.exam_weights = exam_weights
+
+    def hw_average_drop_lowest(self):
+        sorted_hw = np.sort(self.hw, axis=1)[:, 1:]
+        return np.mean(sorted_hw, axis=1)
+
+    def total_score(self):
+        homework_points = self.hw_average_drop_lowest() * self.hw_weight
+        exam_points = np.sum(self.exam * self.exam_weights, axis=1)
+        total_points = exam_points + homework_points
+        return total_points
+
+    def letter_grade(self):
+        grades = self.total_score()
+        grading_scheme = [
+            grades >= 90,
+            grades >= 80,
+            grades >= 70,
+            grades >= 60,
+            grades < 60,
+        ]
+        letters = ["A", "B", "C", "D", "F"]
+        return np.select(grading_scheme, letters)
